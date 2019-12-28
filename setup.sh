9008a51f4c96950e0bd9310ea0626bb487e7d089
Natalia Maximo <iam@natalia.dev>
2019-12-27 20:05:53 -0500

    Added app name to setup

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-echo "Setting up $1..."
+echo "Setting up $2..."
 echo "FLASK_RUN_HOST=0.0.0.0" >> .flaskenv
-echo "FLASK_RUN_PORT=$2" >> .flaskenv
-echo "FLASK_APP=src/$1.py" >> .flaskenv
-echo "SERVICE_NAME=$1" >> .env
-echo "PORT=$2" >> .env
-mv src/samplesvc.py "src/$1.py"
-echo "# $1" > README.md
+echo "FLASK_APP=src/$2.py" >> .flaskenv
+echo "FLASK_RUN_PORT=$3" >> .env
+echo "SERVICE_NAME=$2" >> .env
+echo "APP_NAME=$1" >> .env
+mv src/samplesvc.py "src/$2.py"
+echo "# $2" > README.md
 rm ./setup.sh
 echo "Done!"
 echo "Edit the docker-compose.yaml file to rename the service and"
