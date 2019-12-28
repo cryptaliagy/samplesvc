a46207c5e7b6702c40a7fc6b7fc3b2cad42fd5f9
Natalia Maximo <iam@natalia.dev>
2019-12-27 19:46:24 -0500

    Fixed issue with port

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo "Setting up $1..."
-echo "FLASK_RUN_HOST=0.0.0.0:$2" >> .flaskenv
+echo "FLASK_RUN_HOST=0.0.0.0" >> .flaskenv
+echo "FLASK_RUN_PORT=$2" >> .flaskenv
 echo "FLASK_APP=src/$1.py" >> .flaskenv
 echo "SERVICE_NAME=$1" >> .env
 echo "PORT=$2" >> .env
